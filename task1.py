ea027aca200571696a58b255ba3953c8f137dac3
Mark875 <vasilevskiy.mark@gmail.com>
2024-03-21 16:27:10 +0300

    Added comments to task1

--- task1.py
+++ task1.py
@@ -8,13 +8,18 @@ summa = 0
 
 for p in products:
     category, product, date, price, count = p[0], p[1], p[2], p[3], p[4]
+    # получаем информацию по продукту
     total = float(price) * float(count)
+    # считаем выручку
     f_output.write(";".join([category, product, date, price, count, str(total)]) + "\n")
+    # записываем результат
     if category == "Закуски":
         summa += total
+        # считаем выручку с закусок
 
 
 f_input.close()
 f_output.close()
-
+#закрываем файлы
 print(summa)
+#выводим информацию по закускам
